8b8d8adece5a39abc410ccb69247c7cf1a2666df
Peter Vrenken <peter.vrenken@atos.net>
2022-04-10 23:42:56 +0200

    Added trend.

---
+++ Source/EtAlii.Trends/_Model/Trend.cs
@@ -0,0 +1,5 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Trends
+
+namespace EtAlii.Trends;
+
+public record Trend(string Id, string ParentId, string Name, DateTime Begin, DateTime End);
