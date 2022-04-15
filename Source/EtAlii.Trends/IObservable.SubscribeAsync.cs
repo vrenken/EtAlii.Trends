c017e8ed655df3a1ac3a36bfac4d5af581712e0c
Peter Vrenken <peter.vrenken@atos.net>
2022-04-16 01:27:10 +0200

    Added IObservable.SubscribeAsync.

---
+++ Source/EtAlii.Trends/IObservable.SubscribeAsync.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Trends
+
+namespace EtAlii.Trends;
+
+using System;
+using System.Threading.Tasks;
+
+public static class ObservableSubscribeAsyncExtension
+{
+    public static IDisposable SubscribeAsync<T>(
+        this IObservable<T> source,
+        Func<T,Task> onNext,
+        Action<Exception>? onError = null,
+        Action? onCompleted = null)
+    {
+        void OnNext(T o)
+        {
+            var task = Task.Run(() => onNext(o));
+            task.GetAwaiter().GetResult();
+        }
+
+        if (onError != null && onCompleted != null)
+        {
+            return source.Subscribe(OnNext, onError, onCompleted);
+        }
+        if (onCompleted != null)
+        {
+            return source.Subscribe(OnNext, onCompleted);
+        }
+        return onError != null
+            ? source.Subscribe(OnNext, onError)
+            : source.Subscribe(OnNext);
+    }
+}
