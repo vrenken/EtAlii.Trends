3d3767529bc52f5b75f9ea305164996f0f0d3290
Peter Vrenken <peter.vrenken@atos.net>
2022-04-17 22:17:26 +0200

    Improved Trend POCO.

--- Source/EtAlii.Trends/Editor/Trends/_Model/Trend.cs
+++ Source/EtAlii.Trends/Editor/Trends/_Model/Trend.cs
@@ -10,11 +10,24 @@ public class Trend : DiagramEntity
     public string Name { get; set; } = string.Empty;
     public DateTime Begin { get; set; }
     public DateTime End { get; set; }
+
     public double X { get; set; }
     public double Y { get; set; }
+
+    public double W { get; set; }
+    public double H { get; set; }
 
     public IList<Component> Components { get; init; } = new List<Component>();
 
     public Layer? Layer { get; set; }
     public bool IsExpanded { get; set; }
+
+    public void SetPositionToTime()
+    {
+        var beginAsString = Begin.ToString("yyyyMd");
+        X = int.Parse(beginAsString);
+
+        var endAsString = End.ToString("yyyyMd");
+        H = int.Parse(endAsString) - X;
+    }
 }
