8c5a68f1f00998c99dd36b8e8ecfe59e097971e3
Peter Vrenken <peter.vrenken@atos.net>
2022-04-17 23:36:40 +0200

    Added component adding/editing and removal.

--- Source/EtAlii.Trends/Editor/Trends/TrendPropertyGrid.cs
+++ Source/EtAlii.Trends/Editor/Trends/TrendPropertyGrid.cs
@@ -2,36 +2,142 @@
 
 namespace EtAlii.Trends.Editor.Trends;
 
+using System.Collections.ObjectModel;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Navigations;
 
 public partial class TrendPropertyGrid
 {
-    private List<Component> _components = new();
+    private IList<Component>? _components;
 
-    [Parameter] public Trend? Trend { get; set; }
+    [CascadingParameter(Name = "SelectedTrend")] public Trend? SelectedTrend { get; set; }
 
-    protected override void OnInitialized()
+    [Parameter] public EventCallback<Trend?> SelectedTrendChanged { get; set; }
+
+    private DateTime? _selectedTrendBegin;
+    private DateTime? _selectedTrendEnd;
+
+    protected override Task OnParametersSetAsync()
     {
-        base.OnInitialized();
-        _components.Add(new Component
+        if (SelectedTrend != null)
         {
-            Name = "Inbox",
-        });
-        _components.Add(new Component
+            _selectedTrendBegin = SelectedTrend.Begin;
+            _selectedTrendEnd = SelectedTrend.End;
+            _components = SelectedTrend.Components;
+        }
+        else
         {
-            Name = "Categories",
-        });
-        _components.Add(new Component
+            _selectedTrendBegin = null;
+            _selectedTrendEnd = null;
+            _components = null;
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private async Task UpdateSelectedTrendBegin(DateTime? begin)
+    {
+        if (SelectedTrend != null)
         {
-            Name = "Primary"
-        });
-        _components.Add(new Component
+            SelectedTrend.Begin = begin ?? DateTime.Now;
+
+            await _commandDispatcher
+                .DispatchAsync<Trend>(new UpdateTrendCommand(SelectedTrend!))
+                .ConfigureAwait(false);
+        }
+
+        await SelectedTrendChanged.InvokeAsync(SelectedTrend).ConfigureAwait(false);
+    }
+
+    private async Task UpdateSelectedTrendEnd(DateTime? end)
+    {
+        if (SelectedTrend != null)
         {
-            Name = "Social"
-        });
-        _components.Add(new Component
+            SelectedTrend.End = end ?? DateTime.Now;
+
+            await _commandDispatcher
+                .DispatchAsync<Trend>(new UpdateTrendCommand(SelectedTrend))
+                .ConfigureAwait(false);
+        }
+
+        await SelectedTrendChanged.InvokeAsync(SelectedTrend).ConfigureAwait(false);
+    }
+
+    private async Task OnActionComplete(ActionEventArgs<Component> e)
+    {
+        if (SelectedTrend == null)
         {
-            Name = "Promotions"
-        });
+            e.Cancel = true;
+            return;
+        }
+
+        var component = e.Data;
+        if (component == null)
+        {
+            e.Cancel = true;
+            return;
+        }
+
+        switch (e.RequestType)
+        {
+            case Action.Save:
+                var isNew = component.Id == Guid.Empty;
+                if (isNew)
+                {
+                    await AddComponent(component).ConfigureAwait(false);
+                }
+                else
+                {
+                    await UpdateComponent(component).ConfigureAwait(false);
+                }
+                break;
+            case Action.Delete:
+                await RemoveComponent(component).ConfigureAwait(false);
+                break;
+        }
+    }
+
+    private async Task RemoveComponent(Component component)
+    {
+        var command = new RemoveComponentCommand(component.Id);
+        await _commandDispatcher
+            .DispatchAsync(command)
+            .ConfigureAwait(false);
+
+        _components!.Remove(component);
+
+        await SelectedTrendChanged.InvokeAsync(SelectedTrend).ConfigureAwait(false);
+}
+
+    private async Task UpdateComponent(Component component)
+    {
+        var command = new UpdateComponentCommand(component);
+        await _commandDispatcher
+            .DispatchAsync<Component>(command)
+            .ConfigureAwait(false);
+
+        await SelectedTrendChanged.InvokeAsync(SelectedTrend).ConfigureAwait(false);
+    }
+
+    private async Task AddComponent(Component component)
+    {
+        var command = new AddComponentCommand
+        (
+            Component: trend =>
+            {
+                component.Trend = trend;
+                return component;
+            },
+            TrendId: SelectedTrend.Id
+        );
+        component = await _commandDispatcher
+            .DispatchAsync<Component>(command)
+            .ConfigureAwait(false);
+
+        _components!.Add(component);
+
+        await SelectedTrendChanged.InvokeAsync(SelectedTrend).ConfigureAwait(false);
     }
 }
