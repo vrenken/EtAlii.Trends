fa1d1466ff857c2b2de1bf74c6c976507df05034
Peter Vrenken <peter.vrenken@atos.net>
2022-04-19 01:23:26 +0200

    Corrected typos.

--- Source/EtAlii.Trends/Editor/Trends/Commands/AddComponentCommandHandler.cs
+++ Source/EtAlii.Trends/Editor/Trends/Commands/AddComponentCommandHandler.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Components
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Trends
 
 namespace EtAlii.Trends.Editor.Trends;
 
