29dfaa21925ea98d5fb94e51a1cb093e4181764f
Peter Vrenken <peter.vrenken@atos.net>
2022-04-19 20:26:19 +0200

    Revert to angle/distance pattern.

--- Source/EtAlii.Trends/_Shared/Database/Schema/ConnectionEntityTypeConfiguration.cs
+++ Source/EtAlii.Trends/_Shared/Database/Schema/ConnectionEntityTypeConfiguration.cs
@@ -12,12 +12,12 @@ public class ConnectionEntityTypeConfiguration : IEntityTypeConfiguration<Connec
 
         builder
             .HasOne(entity => entity.Source);
-        builder.Property(entity => entity.SourceBezierX).IsRequired();
-        builder.Property(entity => entity.SourceBezierY).IsRequired();
+        builder.Property(entity => entity.SourceBezierAngle).IsRequired();
+        builder.Property(entity => entity.SourceBezierDistance).IsRequired();
 
         builder
             .HasOne(entity => entity.Target);
-        builder.Property(entity => entity.TargetBezierX).IsRequired();
-        builder.Property(entity => entity.TargetBezierY).IsRequired();
+        builder.Property(entity => entity.TargetBezierAngle).IsRequired();
+        builder.Property(entity => entity.TargetBezierDistance).IsRequired();
     }
 }
