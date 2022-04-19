877e57479912f524ffa1bf6b5e4cdec634da093f
Peter Vrenken <peter.vrenken@atos.net>
2022-04-19 10:48:49 +0200

    Added RemoveTrendCommandHandler.

---
+++ Source/EtAlii.Trends/Editor/Trends/Commands/RemoveTrendCommandHandler.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.Trends
+
+namespace EtAlii.Trends.Editor.Trends;
+
+public record RemoveTrendCommand(Guid TrendId) : AsyncCommand<IRemoveTrendCommandHandler>;
+
+public interface IRemoveTrendCommandHandler : IAsyncCommandHandler<RemoveTrendCommand> {}
+
+public class RemoveTrendCommandHandler : IRemoveTrendCommandHandler
+{
+    public async Task Handle(RemoveTrendCommand command)
+    {
+        // ReSharper disable once UseAwaitUsing
+        using var data = new DataContext();
+
+        var layer = await data.Trends
+            .AsNoTracking()
+            .SingleAsync(l => l.Id == command.TrendId)
+            .ConfigureAwait(false);
+
+        data.Entry(layer).State = EntityState.Deleted;
+        await data
+            .SaveChangesAsync()
+            .ConfigureAwait(false);
+    }
+}
